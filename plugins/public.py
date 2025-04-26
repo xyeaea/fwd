aadc60a2b9827088f1e1ec99ee63268319f7150c
xyeaea <rizqiferdiansyah88@gmail.com>
2025-04-27 01:57:56 +0700

    Update public.py

--- plugins/public.py
+++ plugins/public.py
@@ -1,87 +1,119 @@
 import re
-import asyncio 
+import asyncio
 from .utils import STS
 from database import db
-from config import temp 
+from config import temp
 from translation import Translation
 from pyrogram import Client, filters, enums
-from pyrogram.errors import FloodWait 
-from pyrogram.errors.exceptions.not_acceptable_406 import ChannelPrivate as PrivateChat
-from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified, ChannelPrivate
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
- 
+from pyrogram.errors import FloodWait
+from pyrogram.errors.exceptions import not_acceptable_406, bad_request_400
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery,
+    KeyboardButton,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
 #===================Run Function===================#
 
 @Client.on_message(filters.private & filters.command(["fwd", "forward"]))
 async def run(bot, message):
-    buttons = []
-    btn_data = {}
     user_id = message.from_user.id
     _bot = await db.get_bot(user_id)
+
     if not _bot:
-      return await message.reply("<code>You didn't added any bot. Please add a bot using /settings !</code>")
+        return await message.reply("<code>You didn't add any bot. Please add a bot using /settings!</code>")
+
     channels = await db.get_user_channels(user_id)
     if not channels:
-       return await message.reply_text("please set a to channel in /settings before forwarding")
+        return await message.reply_text("Please set a 'to' channel in /settings before forwarding.")
+
     if len(channels) > 1:
-       for channel in channels:
-          buttons.append([KeyboardButton(f"{channel['title']}")])
-          btn_data[channel['title']] = channel['chat_id']
-       buttons.append([KeyboardButton("cancel")]) 
-       _toid = await bot.ask(message.chat.id, Translation.TO_MSG.format(_bot['name'], _bot['username']), reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, resize_keyboard=True))
-       if _toid.text.startswith(('/', 'cancel')):
-          return await message.reply_text(Translation.CANCEL, reply_markup=ReplyKeyboardRemove())
-       to_title = _toid.text
-       toid = btn_data.get(to_title)
-       if not toid:
-          return await message.reply_text("wrong channel choosen !", reply_markup=ReplyKeyboardRemove())
+        buttons = [
+            [KeyboardButton(channel['title'])] for channel in channels
+        ]
+        buttons.append([KeyboardButton("cancel")])
+        btn_data = {channel['title']: channel['chat_id'] for channel in channels}
+
+        ask_channel = await bot.ask(
+            message.chat.id,
+            Translation.TO_MSG.format(_bot['name'], _bot['username']),
+            reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, resize_keyboard=True)
+        )
+
+        if ask_channel.text.startswith(('/', 'cancel')):
+            return await message.reply_text(Translation.CANCEL, reply_markup=ReplyKeyboardRemove())
+
+        toid = btn_data.get(ask_channel.text)
+        if not toid:
+            return await message.reply_text("Wrong channel chosen!", reply_markup=ReplyKeyboardRemove())
+        to_title = ask_channel.text
     else:
-       toid = channels[0]['chat_id']
-       to_title = channels[0]['title']
-    fromid = await bot.ask(message.chat.id, Translation.FROM_MSG, reply_markup=ReplyKeyboardRemove())
-    if fromid.text and fromid.text.startswith('/'):
-        await message.reply(Translation.CANCEL)
-        return 
-    if fromid.text and not fromid.forward_date:
-        regex = re.compile(r"(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
-        match = regex.match(fromid.text.replace("?single", ""))
+        toid = channels[0]['chat_id']
+        to_title = channels[0]['title']
+
+    ask_source = await bot.ask(message.chat.id, Translation.FROM_MSG, reply_markup=ReplyKeyboardRemove())
+
+    if ask_source.text and ask_source.text.startswith('/'):
+        return await message.reply(Translation.CANCEL)
+
+    chat_id, last_msg_id = None, None
+
+    if ask_source.text and not ask_source.forward_date:
+        regex = re.compile(r"(https://)?(t\.me|telegram\.me|telegram\.dog)/(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)")
+        match = regex.match(ask_source.text.replace("?single", ""))
         if not match:
-            return await message.reply('Invalid link')
+            return await message.reply('Invalid link.')
+        
         chat_id = match.group(4)
         last_msg_id = int(match.group(5))
-        if chat_id.isnumeric():
-            chat_id  = int(("-100" + chat_id))
-    elif fromid.forward_from_chat.type in [enums.ChatType.CHANNEL]:
-        last_msg_id = fromid.forward_from_message_id
-        chat_id = fromid.forward_from_chat.username or fromid.forward_from_chat.id
-        if last_msg_id == None:
-           return await message.reply_text("**This may be a forwarded message from a group and sended by anonymous admin. instead of this please send last message link from group**")
+
+        if chat_id.isdigit():
+            chat_id = int("-100" + chat_id)
+
+    elif ask_source.forward_from_chat and ask_source.forward_from_chat.type == enums.ChatType.CHANNEL:
+        chat_id = ask_source.forward_from_chat.username or ask_source.forward_from_chat.id
+        last_msg_id = ask_source.forward_from_message_id
+
+        if last_msg_id is None:
+            return await message.reply_text("This might be a forwarded message from a group sent by an anonymous admin. Please send the last message link instead.")
     else:
-        await message.reply_text("**invalid !**")
-        return 
+        return await message.reply_text("Invalid input!")
+
+    # Try to get chat title
     try:
         title = (await bot.get_chat(chat_id)).title
-  #  except ChannelInvalid:
-        #return await fromid.reply("**Given source chat is copyrighted channel/group. you can't forward messages from there**")
-    except (PrivateChat, ChannelPrivate, ChannelInvalid):
-        title = "private" if fromid.text else fromid.forward_from_chat.title
-    except (UsernameInvalid, UsernameNotModified):
-        return await message.reply('Invalid Link specified.')
+    except (not_acceptable_406.ChannelPrivate, not_acceptable_406.ChannelPrivate, bad_request_400.ChannelInvalid):
+        title = ask_source.forward_from_chat.title if ask_source.forward_from_chat else "Private"
+    except (bad_request_400.UsernameInvalid, bad_request_400.UsernameNotModified):
+        return await message.reply('Invalid link specified.')
     except Exception as e:
-        return await message.reply(f'Errors - {e}')
+        return await message.reply(f"Error: {e}")
+
     skipno = await bot.ask(message.chat.id, Translation.SKIP_MSG)
+
     if skipno.text.startswith('/'):
-        await message.reply(Translation.CANCEL)
-        return
+        return await message.reply(Translation.CANCEL)
+
     forward_id = f"{user_id}-{skipno.id}"
     buttons = [[
         InlineKeyboardButton('Yes', callback_data=f"start_public_{forward_id}"),
         InlineKeyboardButton('No', callback_data="close_btn")
     ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
+
     await message.reply_text(
-        text=Translation.DOUBLE_CHECK.format(botname=_bot['name'], botuname=_bot['username'], from_chat=title, to_chat=to_title, skip=skipno.text),
+        text=Translation.DOUBLE_CHECK.format(
+            botname=_bot['name'],
+            botuname=_bot['username'],
+            from_chat=title,
+            to_chat=to_title,
+            skip=skipno.text
+        ),
         disable_web_page_preview=True,
-        reply_markup=reply_markup
+        reply_markup=InlineKeyboardMarkup(buttons)
     )
+
+    # Save session temporary
     STS(forward_id).store(chat_id, toid, int(skipno.text), int(last_msg_id))

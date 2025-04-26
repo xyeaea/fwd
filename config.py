409376f75b1c9fdc1e212081f6f404003e2bb2fe
xyeaea <rizqiferdiansyah88@gmail.com>
2025-04-27 01:55:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
-from os import environ 
+import os
+from typing import List
 
 class Config:
-    API_ID = environ.get("API_ID", "22412440")
-    API_HASH = environ.get("API_HASH", "211165a095cd129a58bc1d23515c01ef")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7578490063:AAHQDkFc_oBkTiAUlMVGcj4ferI0rb0DVrk") 
-    BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://ackbot:ackbot@bot.uthcly0.mongodb.net/?retryWrites=true&w=majority&appName=bot")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '5505135072').split()]
-
-class temp(object): 
-    lock = {}
-    CANCEL = {}
-    forwardings = 0
-    BANNED_USERS = []
-    IS_FRWD_CHAT = []
+    API_ID: int = int(os.getenv("API_ID", "22412440"))
+    API_HASH: str = os.getenv("API_HASH", "211165a095cd129a58bc1d23515c01ef")
+    BOT_TOKEN: str = os.getenv("BOT_TOKEN", "7578490063:AAHQDkFc_oBkTiAUlMVGcj4ferI0rb0DVrk")
+    BOT_SESSION: str = os.getenv("BOT_SESSION", "bot")
+    DATABASE_URI: str = os.getenv("DATABASE", "mongodb+srv://ackbot:ackbot@bot.uthcly0.mongodb.net/?retryWrites=true&w=majority&appName=bot")
+    DATABASE_NAME: str = os.getenv("DATABASE_NAME", "forward-bot")
     
+    BOT_OWNER_ID: List[int] = [int(x) for x in os.getenv("BOT_OWNER_ID", "5505135072").split()]
+
+class Temp:
+    lock: dict = {}
+    cancel: dict = {}
+    forwardings: int = 0
+    banned_users: List[int] = []
+    is_frwd_chat: List[int] = []
